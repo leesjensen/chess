d46e4fa93a97e6670c9c9793c2bb25cea2f02a8a
Lee <leesjensen@gmail.com>
2026-09-11 10:04:20 -0600

    impl(bishop) first test passing

---
+++ shared/src/main/java/chess/Bishop.java
@@ -0,0 +1,7 @@
+package chess;
+
+public class Bishop extends ChessPiece {
+    public Bishop(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        super(pieceColor, type);
+    }
+}
